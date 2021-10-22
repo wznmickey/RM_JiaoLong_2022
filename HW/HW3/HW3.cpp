99ef31ce9c02fe67922400c4813a83bf616c231a
wznmickey <first@wznmickey.com>
2021-10-22 20:14:43 +0800

    HW3/2

--- HW/HW3/HW3.cpp
+++ HW/HW3/HW3.cpp
@@ -90,23 +90,113 @@ std::vector<cv::Point> getPosition(Mat src)
 }
 int main()
 {
-    Mat img = imread("data pack/car.jpg");
-    Mat result;
+    int x;
+    cin >>x;
+    if (x == 1)
+    {
+        Mat img = imread("data pack/car.jpg");
+        Mat result;
 
-    std::vector<cv::Point> ans = getPosition(img);
-    doPerspectiveTransform(img, result, ans);
-    transpose(result, result);
-    flip(result, result, 1);
+        std::vector<cv::Point> ans = getPosition(img);
+        doPerspectiveTransform(img, result, ans);
+        transpose(result, result);
+        flip(result, result, 1);
 
-    namedWindow("input");
-    namedWindow("output");
+        namedWindow("input");
+        namedWindow("output");
 
-    imshow("input", img);
-    imshow("output", result);
+        imshow("input", img);
+        imshow("output", result);
 
-    waitKey(0);
+        waitKey(0);
 
-    destroyWindow("input");
-    destroyWindow("output");
+        destroyWindow("input");
+        destroyWindow("output");
+    }
+    if (x == 2)
+    {
+        const int board_w = 9,
+                  board_h = 6;
+        const int board_n = board_w * board_h;
+        Size board_size(9, 6);
+        Mat gray_img, drawn_img;
+        std::vector<Point2f> point_pix_pos_buf;
+        int found, successes = 0;
+        Size img_size;
+        int cnt = 0;
+        int k = 0, n = 0;
+
+        for (int i = 0; i < 41; i++)
+        {
+            //std::cout<<3<<std::endl;
+            std::string path = "data pack/chess/";
+            cv::Mat src0;
+            cv::Mat src = cv::imread(path.append((std::__cxx11::to_string(i).append(".jpg"))));
+
+            //std::cout<<src0;
+            //std::cout<<4<<std::endl;
+            cv::Mat src1;
+            cv::cvtColor(src, src1, cv::COLOR_BGR2GRAY);
+            //std::cout<<5<<std::endl;
+            cv::Mat src2;
+
+            cv::adaptiveThreshold(src1, src0, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 87, 0);
+            // std::cout<<6<<std::endl;
+
+            //cv::imshow("result", src0);
+            //cv::waitKey(0);
+            if (!cnt)
+            {
+                img_size.width = src0.cols;
+                img_size.height = src0.rows;
+            }
+            found = findChessboardCorners(src0, board_size, point_pix_pos_buf);
+            //std::cout<<found<<std::endl;
+            if (found && point_pix_pos_buf.size() == board_n)
+            {
+                successes++;
+                //cvtColor(src0, gray_img, COLOR_BGR2GRAY);
+                find4QuadCornerSubpix(src0, point_pix_pos_buf, Size(5, 5));
+                drawn_img = src0.clone();
+                drawChessboardCorners(drawn_img, board_size, point_pix_pos_buf, found);
+                cv::Mat show;
+                cv::resize(drawn_img, show, Size(960, 540));
+                imshow("corners", show);
+                waitKey(50);
+            //std::cout << "1" << std::endl;
+            //std::cout << "2" << std::endl;
+            cnt++;
+
+            std::cout << successes << " useful chess boards" << std::endl;
+            Size square_size(10, 10);
+            std::vector<Point3f> point_grid_pos_buf;
+            Mat camera_matrix = cv::Mat_<int>(3, 3) << (9.1234465679824348e+02, 0., 6.4157634413436961e+02, 0.,
+                                                        7.6573154962089018e+02, 3.6477945186797331e+02, 0., 0., 1.);
+            Mat dist_coeffs = cv::Mat_<int>(1, 5) << (0., -4.2669718747763807e-01, 2.6509688616309912e-01,
+                                                      -5.3624979910268683e-04, -4.1011485564833132e-04);
+            Mat rvecs;
+            Mat tvecs;
+
+            for (int j = 0; j < board_h; j++)
+            {
+                for (int k = 0; k < board_w; k++)
+                {
+                    Point3f pt;
+                    pt.x = k * square_size.width;
+                    pt.y = j * square_size.height;
+                    pt.z = 0;
+                    point_grid_pos_buf.push_back(pt);
+                }
+            }
+            solvePnP(point_grid_pos_buf, point_pix_pos_buf, camera_matrix, dist_coeffs, rvecs, tvecs);
+            point_pix_pos_buf.clear();
+            point_grid_pos_buf.clear();
+            cout << rvecs << endl;
+                        }
+            else
+                std::cout << "failed in" << i << std::endl;
+
+        }
+    }
     return 0;
 }
